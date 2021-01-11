14208774d46980f7d745e630b4353840c16e0663
hide-sugi <hsdgot711711@gmail.com>
2021-01-11 22:33:56 +0900

    コントローラーの修正

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,25 +1,24 @@
-class DonationsController < ApplicationController
+class  DonationsController < ApplicationController
   def index
   end
-
+ 
   def new
-    @user_donation = UserDonation.new
+    @user_donation = UserDonation.new   #「UserDonation」に編集
   end
-
+ 
   def create
     @user_donation = UserDonation.new(donation_params)   #「UserDonation」に編集
-    if @user_donation.valid?
-      @user_donation.save
-      redirect_to action: :index
-    else
-      render action: :new
-    end
+     if @user_donation.valid?
+       @user_donation.save
+       redirect_to action: :index
+     else
+       render action: :new
+     end
   end
-
+ 
   private
-
   def donation_params
-    params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
+   params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
   end
-
-end
+ 
+ end
