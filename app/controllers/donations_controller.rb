ecc40ad581860e208a7423edd305296fa5943368
hide-sugi <hsdgot711711@gmail.com>
2021-01-21 22:11:56 +0900

    donations_controller.rの修正

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,11 +1,11 @@
 class  DonationsController < ApplicationController
   def index
   end
- 
+
   def new
     @user_donation = UserDonation.new
   end
- 
+
   def create
     @user_donation = UserDonation.new(donation_params)
      if @user_donation.valid?
@@ -15,7 +15,7 @@ class  DonationsController < ApplicationController
        render action: :new
      end
   end
- 
+
   private
   def donation_params
    params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
